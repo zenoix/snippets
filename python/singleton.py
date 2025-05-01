74905ecddddc4897a4bd129d8b4844e5d6063535
zenoix <41812358+zenoix@users.noreply.github.com>
2025-05-01 16:34:39 +1200

    feat(python/singleton): make singleton thread-safe

--- python/singleton.py
+++ python/singleton.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from threading import Lock
 from typing import Any, Generic, TypeVar
 
 T = TypeVar("T")
@@ -8,7 +9,10 @@ T = TypeVar("T")
 class Singleton(type, Generic[T]):
     _instances: dict[Singleton[T], T] = {}  # noqa: RUF012
 
+    _lock: Lock = Lock()
+
     def __call__(cls, *args: Any, **kwargs: Any) -> T:  # noqa: ANN401
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
