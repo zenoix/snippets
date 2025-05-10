06585cf2ee02849e9195734c02e3a763280e5434
zenoix <41812358+zenoix@users.noreply.github.com>
2025-05-10 18:40:43 +1200

    feat(python/lazily-evaluated-env-vars): add float and int settings

--- python/lazily-evaluated-env-vars.py
+++ python/lazily-evaluated-env-vars.py
@@ -1,7 +1,9 @@
+import numbers
 import os
 from collections.abc import Callable, Iterable, MutableSequence, Sequence
 from dataclasses import KW_ONLY, dataclass
 from functools import cached_property
+from math import ceil, floor, trunc
 from typing import Any, ClassVar, Never, Self, TypeVar, cast, overload, override
 
 type Missing = object
@@ -87,6 +89,338 @@ class Setting(Sequence[str]):
     @override
     def __getitem__(self, index: int | slice) -> str:
         return self.value[index]
+
+
+@dataclass
+class FloatSetting(numbers.Real):
+    """Lazily evaluated class representing a float environment variable.
+
+    Args:
+        name: The name of the environment variable
+        default: The default value of the environment variable
+
+    Attributes:
+        value: Eagerly evaluate the value of the environment variable
+    """
+
+    name: str
+    _ = KW_ONLY
+    default: str | Missing = MISSING
+
+    def __post_init__(self) -> None:
+        if isinstance(self.default, str) or self.default is MISSING:
+            return
+        msg = (
+            "Setting's `default` argument must be a string if provided, not "
+            f"{type(self.default).__name__}"
+        )
+        raise TypeError(msg)
+
+    def __get__(self, obj: object, objtype: type | None = None) -> float:
+        """Evaluate the value of the environment variable when accessed.
+
+        Returns:
+            The value of the environment variable
+        """
+        return self.value
+
+    @cached_property
+    def value(self) -> float:
+        """Evaluate the value of the environment variable.
+
+        Returns:
+            The value of the environment variable
+        """
+        if self.default is MISSING:
+            value = os.environ[self.name]
+        value = cast("str", os.environ.get(self.name, self.default))
+
+        try:
+            return float(value)
+        except ValueError as err:
+            msg = f"Environment variable '{self.name}' with value '{value}' cannot be converted to a float"
+            raise ValueError(msg) from err
+
+    @override
+    def __abs__(self) -> float:
+        return abs(self.value)
+
+    @override
+    def __add__(self, other: object) -> float | complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value + other.value
+        if isinstance(other, complex | float | int):
+            return self.value + other
+        return NotImplemented
+
+    @override
+    def __ceil__(self) -> int:
+        return ceil(self.value)
+
+    @override
+    def __float__(self) -> float:
+        return self.value
+
+    @override
+    def __floor__(self) -> int:
+        return floor(self.value)
+
+    @override
+    def __floordiv__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value // other.value
+        return self.value // other
+
+    @override
+    def __le__(self, other: "float | FloatSetting | IntSetting") -> bool:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value <= other.value
+        return self.value <= other
+
+    @override
+    def __lt__(self, other: "float | FloatSetting | IntSetting") -> bool:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value < other.value
+        return self.value < other
+
+    @override
+    def __mod__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value % other.value
+        return self.value % other
+
+    @override
+    def __mul__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value * other.value
+        return self.value * other
+
+    @override
+    def __neg__(self) -> float:
+        return -self.value
+
+    @override
+    def __pos__(self) -> float:
+        return self.value
+
+    @override
+    def __pow__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value**other.value
+        return self.value**other
+
+    @overload
+    def __round__(self, ndigits: None) -> int: ...
+
+    @overload
+    def __round__(self, ndigits: int) -> float: ...
+
+    @override
+    def __round__(self, ndigits: int | None = None) -> float | int:
+        return round(self.value, ndigits)
+
+    @override
+    def __truediv__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value / other.value
+        return self.value / other
+
+    @override
+    def __trunc__(self) -> int:
+        return trunc(self.value)
+
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+    @override
+    def __rfloordiv__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value // self.value
+        return other // self.value
+
+    @override
+    def __rmod__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value % self.value
+        return other % self.value
+
+    @override
+    def __rpow__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value**self.value
+        return other**self.value
+
+    @override
+    def __rtruediv__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value / self.value
+        return other / self.value
+
+
+@dataclass
+class IntSetting(numbers.Real):
+    """Lazily evaluated class representing a integer environment variable.
+
+    Args:
+        name: The name of the environment variable
+        default: The default value of the environment variable
+
+    Attributes:
+        value: Eagerly evaluate the value of the environment variable
+    """
+
+    name: str
+    _ = KW_ONLY
+    default: str | Missing = MISSING
+
+    def __post_init__(self) -> None:
+        if isinstance(self.default, str) or self.default is MISSING:
+            return
+        msg = (
+            "Setting's `default` argument must be a string if provided, not "
+            f"{type(self.default).__name__}"
+        )
+        raise TypeError(msg)
+
+    def __get__(self, obj: object, objtype: type | None = None) -> int:
+        """Evaluate the value of the environment variable when accessed.
+
+        Returns:
+            The value of the environment variable
+        """
+        return self.value
+
+    @cached_property
+    def value(self) -> int:
+        """Evaluate the value of the environment variable.
+
+        Returns:
+            The value of the environment variable
+        """
+        if self.default is MISSING:
+            value = os.environ[self.name]
+        value = cast("str", os.environ.get(self.name, self.default))
+
+        try:
+            return int(value)
+        except ValueError as err:
+            msg = f"Environment variable '{self.name}' with value '{value}' cannot be converted to an int"
+            raise ValueError(msg) from err
+
+    @override
+    def __abs__(self) -> int:
+        return abs(self.value)
+
+    @override
+    def __add__(self, other: object) -> complex:
+        if isinstance(other, IntSetting | FloatSetting):
+            return self.value + other.value
+        if isinstance(other, complex | float | int):
+            return self.value + other
+        return NotImplemented
+
+    @override
+    def __ceil__(self) -> int:
+        return ceil(self.value)
+
+    @override
+    def __float__(self) -> float:
+        return self.value
+
+    @override
+    def __floor__(self) -> int:
+        return floor(self.value)
+
+    @override
+    def __floordiv__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value // other.value
+        return self.value // other
+
+    @override
+    def __le__(self, other: "float | FloatSetting | IntSetting") -> bool:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value <= other.value
+        return self.value <= other
+
+    @override
+    def __lt__(self, other: "float | FloatSetting | IntSetting") -> bool:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value < other.value
+        return self.value < other
+
+    @override
+    def __mod__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value % other.value
+        return self.value % other
+
+    @override
+    def __mul__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value * other.value
+        return self.value * other
+
+    @override
+    def __neg__(self) -> int:
+        return -self.value
+
+    @override
+    def __pos__(self) -> int:
+        return self.value
+
+    @override
+    def __pow__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value**other.value
+        return self.value**other
+
+    @overload
+    def __round__(self, ndigits: None) -> int: ...
+
+    @overload
+    def __round__(self, ndigits: int) -> float: ...
+
+    @override
+    def __round__(self, ndigits: int | None = None) -> int:
+        return round(self.value, ndigits)
+
+    @override
+    def __truediv__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return self.value / other.value
+        return self.value / other
+
+    @override
+    def __trunc__(self) -> int:
+        return trunc(self.value)
+
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+    @override
+    def __rfloordiv__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value // self.value
+        return other // self.value
+
+    @override
+    def __rmod__(self, other: "float | FloatSetting | IntSetting") -> float:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value % self.value
+        return other % self.value
+
+    @override
+    def __rpow__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value**self.value
+        return other**self.value
+
+    @override
+    def __rtruediv__(self, other: "complex | FloatSetting | IntSetting") -> complex:
+        if isinstance(other, FloatSetting | IntSetting):
+            return other.value / self.value
+        return other / self.value
 
 
 @dataclass
